beedc1b973055ab82ce879708cd3cd77d5faaae0
chenxinjie <504631221@qq.com>
2021-12-24 19:12:40 +0800

    提交所有的

--- src/main/java/com/chen.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class chen {
-    public static void main(String[] args) {
-        System.out.println("==0000000000======999999=============================");
-        System.out.println("========999999======11113333333=========bfbdd==============");
-    }
-}
--- src/main/java/com/chen2.java
+++ src/main/java/com/chen2.java
@@ -2,6 +2,6 @@ package com;
 
 public class chen2 {
     public static void main(String[] args) {
-        System.out.println("22222222222222222222222222223333333333333333333333333");
+        System.out.println("22222222244422222222222222222223333333333333333333333333");
     }
 }
