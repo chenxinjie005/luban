abfda8b9eaaa4b19863c6afef8d5a2acf1d5a4b0
chenxinjie005 <504631221@qq.com>
2022-01-09 21:09:04 +0800

    gggg

--- src/main/java/com/chen2.java
+++ src/main/java/com/chen2.java
@@ -2,10 +2,8 @@ package com;
 
 public class chen2 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("22222222244422225552222222222222223333333333333333333333333");
-=======
         System.out.println("22222222244422222222222222222223333333333333333333333333");
->>>>>>> beedc1b973055ab82ce879708cd3cd77d5faaae0
+        System.out.println("dev9999999999999999999999999999999999999999999999999999999");
     }
 }
